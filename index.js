64b0a9ed991b817b65f95ac58ead18d1121d712f
ZaforAbullah <zafor@vialing.com>
2021-03-12 05:52:15 +0800

    Instance Lifecycle Hooks

--- index.js
+++ index.js
@@ -15,3 +15,14 @@ new Vue({
   el: '#app-2',
   data: obj
 })
+
+new Vue({
+  el: '#app-3',
+  data: {
+    a: 1
+  },
+  created: function () {
+    // `this` points to the vm instance
+    console.log('a is: ' + this.a)
+  }
+})
