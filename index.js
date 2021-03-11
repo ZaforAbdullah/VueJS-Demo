2114e9b29b2663ee6f0e01f69b5c814a758f9ed2
ZaforAbullah <zafor@vialing.com>
2021-03-12 06:03:27 +0800

    Interpolations with Text

--- index.js
+++ index.js
@@ -5,24 +5,3 @@ var vm = new Vue({
   }
 })
 
-var obj = {
-  foo: 'bar'
-}
-
-Object.freeze(obj)
-
-new Vue({
-  el: '#app-2',
-  data: obj
-})
-
-new Vue({
-  el: '#app-3',
-  data: {
-    a: 1
-  },
-  created: function () {
-    // `this` points to the vm instance
-    console.log('a is: ' + this.a)
-  }
-})
