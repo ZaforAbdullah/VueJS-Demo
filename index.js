2c094b4cf95eb86e9fee493f8592164cd46920b5
ZaforAbullah <zafor@vialing.com>
2021-03-12 05:47:02 +0800

    Data and Methods

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var vm = new Vue({
-  el: '#app',
+  el: '#app-1',
   data: {
     message: 'Hello Vue!'
   }
+})
+
+var obj = {
+  foo: 'bar'
+}
+
+Object.freeze(obj)
+
+new Vue({
+  el: '#app-2',
+  data: obj
 })
