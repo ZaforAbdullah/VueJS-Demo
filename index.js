d0f2f794551624598bbe14c1cbe2c6eb8f5e7e69
ZaforAbullah <zafor@vialing.com>
2021-03-12 23:48:16 +0800

    Mapping an Array to Elements with v-for

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-var vm = new Vue({
-    el: '#example-1',
-    data: {
-        a: false,
-        b: true,
-		ok: true
-    }
-});
+var example1 = new Vue({
+  el: '#example-1',
+  data: {
+    items: [
+      { message: 'Foo' },
+      { message: 'Bar' }
+    ]
+  }
+})
 
-
-
-
-
+var example2 = new Vue({
+  el: '#example-2',
+  data: {
+    parentMessage: 'Parent',
+    items: [
+      { message: 'Foo' },
+      { message: 'Bar' }
+    ]
+  }
+})
