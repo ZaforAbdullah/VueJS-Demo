46facf9c97186653946f1b3d1353dda8dd1ee5ec
ZaforAbullah <zafor@vialing.com>
2021-03-12 22:19:21 +0800

    v-if

--- index.js
+++ index.js
@@ -1,46 +1,10 @@
-var vm1 = new Vue({
-  el: '#example-1',
-  data: {
-	isActive: true,
-	hasError: false
-  }
-})
-
-var vm1 = new Vue({
-  el: '#example-2',
-  data: {
-	isActive: true,
-	error: null
-  },
-  computed: {
-	classObject: function () {
-	  return {
-		active: this.isActive && !this.error,
-		'text-danger': this.error && this.error.type === 'fatal'
-	  }
-	}
-  }
-})
-
-Vue.component('my-component', {
-  template: '<p class="foo bar">Hi</p>'
-})
-
-var vm1 = new Vue({
-  el: '#example-3',
-  data: {
-	activeClass: 'active',
-	errorClass: 'text-danger'
-  }
-})
-
-var vm1 = new Vue({
-  el: '#example-4',
-  data: {
-   activeColor: 'red',
-   fontSize: 30
- }
-})
+var vm = new Vue({
+    el: '#example-1',
+    data: {
+        a: true,
+        b: false
+    }
+});
 
 
 
