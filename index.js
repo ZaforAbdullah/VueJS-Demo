5bcb61b3f661282baa8a1a168950d90f7e1072d0
ZaforAbullah <zafor@vialing.com>
2021-03-12 23:58:26 +0800

    v-for with an Object

--- index.js
+++ index.js
@@ -18,3 +18,14 @@ var example2 = new Vue({
     ]
   }
 })
+
+var example3 = new Vue({
+  el: '#v-for-object',
+  data: {
+    object: {
+      title: 'How to do lists in Vue',
+      author: 'Jane Doe',
+      publishedAt: '2016-04-10'
+    }
+  }
+})
