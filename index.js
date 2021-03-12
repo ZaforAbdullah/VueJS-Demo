9ba4d1e8874c546ad5c60b815d1cf79c8d720146
ZaforAbullah <zafor@vialing.com>
2021-03-12 18:41:47 +0800

    Computed vs Watched Property

--- index.js
+++ index.js
@@ -26,4 +26,34 @@ var vm = new Vue({
   }
 })
 
+var vm = new Vue({
+  el: '#example-3',
+  data: {
+    firstName: 'Foo',
+    lastName: 'Bar',
+    fullName: 'Foo Bar'
+  },
+  watch: {
+    firstName: function (val) {
+      this.fullName = val + ' ' + this.lastName
+    },
+    lastName: function (val) {
+      this.fullName = this.firstName + ' ' + val
+    }
+  }
+})
+
+var vm = new Vue({
+  el: '#example-4',
+  data: {
+    firstName: 'Foo',
+    lastName: 'Bar'
+  },
+  computed: {
+    fullName: function () {
+      return this.firstName + ' ' + this.lastName
+    }
+  }
+})
+
 
