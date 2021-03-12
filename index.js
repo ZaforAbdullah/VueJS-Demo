b5747e0bcb9762c4fa323e3ece1d1355e54fc5df
ZaforAbullah <zafor@vialing.com>
2021-03-13 00:09:43 +0800

    Displaying Filtered/Sorted Results

--- index.js
+++ index.js
@@ -29,3 +29,31 @@ var example3 = new Vue({
     }
   }
 })
+
+var example4 = new Vue({
+  el: '#example-3',
+  data: {
+    numbers: [ 1, 2, 3, 4, 5 ]
+  },
+  computed: {
+    evenNumbers: function () {
+      return this.numbers.filter(function (number) {
+        return number % 2 === 0
+       })
+    }
+  }
+})
+
+var example = new Vue({
+  el: '#example-4',
+  data: {
+    sets: [[ 1, 2, 3, 4, 5 ], [6, 7, 8, 9, 10]]
+  },
+  methods: {
+    even: function (numbers) {
+      return numbers.filter(function (number) {
+        return number % 2 === 0
+      })
+    }
+  }
+})
