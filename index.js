7163a76c2250d3dfd291f66cfb94adf742da1f8c
ZaforAbullah <zafor@vialing.com>
2021-03-13 01:09:51 +0800

    Methods in Inline Handlers

--- index.js
+++ index.js
@@ -22,3 +22,19 @@ var example2 = new Vue({
     }
   }
 })
+
+new Vue({
+  el: '#example-3',
+  methods: {
+    say: function (message) {
+      alert(message)
+    },
+	 warn: function (message, event) {
+    // now we have access to the native event
+    if (event) {
+      event.preventDefault()
+    }
+       alert(message)
+	}
+  }
+})
