fb6be888c27513fceaec10a731db787705890586
ZaforAbullah <zafor@vialing.com>
2021-03-13 02:18:38 +0800

    Passing Data to Child Components with Props

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ Vue.component('button-counter', {
   template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
 })
 
+Vue.component('blog-post', {
+  props: ['title'],
+  template: '<h3>{{ title }}</h3>'
+})
+
 var example1 = new Vue({
   el: '#components-demo',
   data: {
@@ -19,5 +24,23 @@ var example1 = new Vue({
   el: '#components-demo-1',
   data: {
     message: 'message'
+  }
+})
+
+var example2 = new Vue({
+  el: '#components-demo-2',
+  data: {
+    message: 'message'
+  }
+})
+
+var example3 = new Vue({
+  el: '#components-demo-3',
+  data: {
+    posts: [
+      { id: 1, title: 'My journey with Vue' },
+      { id: 2, title: 'Blogging with Vue' },
+      { id: 3, title: 'Why Vue is so fun' }
+    ]
   }
 })
