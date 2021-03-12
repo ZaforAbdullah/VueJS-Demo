560c83cf9bcf7605993bd67b5a7cdc0c9a069286
ZaforAbullah <zafor@vialing.com>
2021-03-12 21:36:45 +0800

    Binding HTML Classes (computed property)

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ var vm1 = new Vue({
   }
 })
 
+var vm1 = new Vue({
+  el: '#example-2',
+  data: {
+	isActive: true,
+	error: null
+  },
+  computed: {
+	classObject: function () {
+	  return {
+		active: this.isActive && !this.error,
+		'text-danger': this.error && this.error.type === 'fatal'
+	  }
+	}
+  }
+})
 
 
 
