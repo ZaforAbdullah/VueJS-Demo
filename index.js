cdc451be6f0fbf3d9b8ed800543b99456d280538
ZaforAbullah <zafor@vialing.com>
2021-03-12 18:25:26 +0800

    Computed Properties

--- index.js
+++ index.js
@@ -1,65 +1,13 @@
-var vm1 = new Vue({
-  el: '#app-1',
+var vm = new Vue({
+  el: '#example',
   data: {
-    message: 'Hello Vue!'
-  }
-})
-
-
-var vm2 = new Vue({
-  el: '#app-2',
-  data: {
-    message: '<h1>HELLO '+ 
-                           '<span style="color:blue;">'+ 
-                              'WORLD</span>'+ 
-                         '</h1>'
-  }
-})
-
-var vm3 = new Vue({
-  el: '#app-3',
-  data: {
-    message: 'Hello Vue!'
-  }
-})
-
-var vm4 = new Vue({
-  el: '#app-4',
-  data: {
-    message: 'Hello Vue!',
-	seen: true
-  }
-})
-
-var vm5 = new Vue({
-  el: '#app-5',
-  data: {
-    message: 'Hello Vue!',
-	seen: true,
-	url: 'https://www.facebook.com/'
+    message: 'Hello'
   },
-    methods: {
-    alerttest: function() {
-      alert('test')
-    }
-  }
-})
-
-var vm6 = new Vue({
-  el: '#app-6',
-  data: {
-	url: 'https://www.facebook.com/'
-  }
-})
-
-var vm7 = new Vue({
-  el: '#app-7',
-  data: {
-	url: 'https://www.facebook.com/'
-  },
-    methods: {
-    alerttest: function() {
-      alert('test')
+  computed: {
+    // a computed getter
+    reversedMessage: function () {
+      // `this` points to the vm instance
+      return this.message.split('').reverse().join('')
     }
   }
 })
