d5140c207234fd40c07733937a26fbc807a553d1
ZaforAbullah <zafor@vialing.com>
2021-03-13 00:50:05 +0800

    complete example of a simple todo list

--- index.js
+++ index.js
@@ -1,59 +1,40 @@
-var example1 = new Vue({
-  el: '#example-1',
-  data: {
-    items: [
-      { message: 'Foo' },
-      { message: 'Bar' }
-    ]
-  }
+Vue.component('todo-item', {
+  template: '\
+    <li>\
+      {{ title }}\
+      <button v-on:click="$emit(\'remove\')">Remove</button>\
+    </li>\
+  ',
+  props: ['title']
 })
 
-var example2 = new Vue({
-  el: '#example-2',
+new Vue({
+  el: '#todo-list-example',
   data: {
-    parentMessage: 'Parent',
-    items: [
-      { message: 'Foo' },
-      { message: 'Bar' }
-    ]
-  }
-})
-
-var example3 = new Vue({
-  el: '#v-for-object',
-  data: {
-    object: {
-      title: 'How to do lists in Vue',
-      author: 'Jane Doe',
-      publishedAt: '2016-04-10'
-    }
-  }
-})
-
-var example4 = new Vue({
-  el: '#example-3',
-  data: {
-    numbers: [ 1, 2, 3, 4, 5 ]
-  },
-  computed: {
-    evenNumbers: function () {
-      return this.numbers.filter(function (number) {
-        return number % 2 === 0
-       })
-    }
-  }
-})
-
-var example = new Vue({
-  el: '#example-4',
-  data: {
-    sets: [[ 1, 2, 3, 4, 5 ], [6, 7, 8, 9, 10]]
+    newTodoText: '',
+    todos: [
+      {
+        id: 1,
+        title: 'Do the dishes',
+      },
+      {
+        id: 2,
+        title: 'Take out the trash',
+      },
+      {
+        id: 3,
+        title: 'Mow the lawn'
+      }
+    ],
+    nextTodoId: 4
   },
   methods: {
-    even: function (numbers) {
-      return numbers.filter(function (number) {
-        return number % 2 === 0
+    addNewTodo: function () {
+      this.todos.push({
+        id: this.nextTodoId++,
+        title: this.newTodoText
       })
+      this.newTodoText = ''
     }
   }
 })
