1bbf4731456a066887cb32b8c97424ed2b8370ac
ZaforAbullah <zafor@vialing.com>
2021-03-14 03:32:43 +0800

    Listening to Child Components Events

--- index.js
+++ index.js
@@ -65,3 +65,28 @@ var example4 = new Vue({
     ]
   }
 })
+
+Vue.component('blog-postss', {
+  props: ['post'],
+  template: `
+    <div class="blog-post">
+      <h3>{{ post.title }}</h3>
+      <button v-on:click="$emit('enlarge-text')">
+		Enlarge text
+	  </button>
+      <div v-html="post.content"></div>
+    </div>
+  `
+})
+
+var example5 = new Vue({
+  el: '#blog-posts-events-demo',
+  data: {
+    posts: [
+      { id: 1, title: 'My journey with Vue', content: '<b> First Content </br>', publishedAt: 'Today', comments: 'good'},
+      { id: 2, title: 'Blogging with Vue', content: '<b> Second Content </br>', publishedAt: 'Today', comments: 'good'},
+      { id: 3, title: 'Why Vue is so fun', content: '<b> Third Content </br>', publishedAt: 'Today', comments: 'good'}
+    ],
+	postFontSize: 1
+  }
+})
