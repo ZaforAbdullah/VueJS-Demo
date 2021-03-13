0435e4908343cf1db351c6116cb0ac444c4285c5
ZaforAbullah <zafor@vialing.com>
2021-03-14 02:48:29 +0800

    A Single Root Element

--- index.js
+++ index.js
@@ -44,3 +44,24 @@ var example3 = new Vue({
     ]
   }
 })
+
+Vue.component('blog-posts', {
+  props: ['post'],
+  template: `
+    <div class="blog-post">
+      <h3>{{ post.title }}</h3>
+      <div v-html="post.content"></div>
+    </div>
+  `
+})
+
+var example4 = new Vue({
+  el: '#components-demo-4',
+  data: {
+    posts: [
+      { id: 1, title: 'My journey with Vue', content: '<b> First Content </br>', publishedAt: 'Today', comments: 'good'},
+      { id: 2, title: 'Blogging with Vue', content: '<b> Second Content </br>', publishedAt: 'Today', comments: 'good'},
+      { id: 3, title: 'Why Vue is so fun', content: '<b> Third Content </br>', publishedAt: 'Today', comments: 'good'}
+    ]
+  }
+})
