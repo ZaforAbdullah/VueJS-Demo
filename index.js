afea2e46bd0f333d14da2c11fa0bab3c66d5fbc3
ZaforAbullah <abuzaforabd@gmail.com>
2021-04-04 06:21:04 +0800

    Scoped Slots

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-Vue.component('navigation-link', {
-	props: ['url'],
-	template: `
-		<div class="container">
-		  <header>
-			<slot name="header">Header</slot>
-		  </header>
-		  <main>
-			<slot>Main</slot>
-		  </main>
-		  <footer>
-			<slot name="footer">Footer</slot>
-		  </footer>
-		</div>
-	`
-})
+Vue.component('my-list', {
+  template: '#my-list',
+  props: [ 'title', 'items' ]
+});
 
-var example1 = new Vue({
-	el: '#demo-1',
-})
-
-
+new Vue({
+  el: '#app',
+  data: {
+	 Shapes: 'Shapes',
+	 Colors: 'Colors',
+    shapes: [ 
+      { name: 'Square', sides: 4 }, 
+      { name: 'Hexagon', sides: 6 }, 
+      { name: 'Triangle', sides: 3 }
+    ],
+    colors: [
+      { name: 'Yellow', hex: '#F4D03F', },
+      { name: 'Green', hex: '#229954' },
+      { name: 'Purple', hex: '#9B59B6' }
+    ]
+  }
+});
